dd35885dac9648784abafc48e1bcdd8d9d0b19c3
yash <yporwal333@gmail.com>
2024-12-05 22:45:57 +0530

    section responsive css

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,4 +9,28 @@ $(document).ready(function () {
     if (window.matchMedia("(max-width: 767px)").matches) {
         $("header.header .right-part .button-1").appendTo("header.header .navbar-nav");
     }
+
+    if (window.matchMedia("(max-width: 767px)").matches) {
+    $('.coffee-style-section .coffee-style-slider, .comparision-section .comparision-slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        // variableWidth: true,
+        dots: true,
+        arrows:false,
+        infinite: true,
+        centerMode: false,
+        focusOnSelect: true,
+        responsive: [
+            {
+              breakpoint: 576,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+              }
+            },
+        ]
+      });
+    }
 });
